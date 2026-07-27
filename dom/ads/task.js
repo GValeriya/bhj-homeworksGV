2f8e47ce5edba8fabb9d3b59addef913584c2cb2
SeraV <garanina_valeriya@mail.ru>
2026-07-27 15:36:41 +0500

    second ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,14 @@
-const rotator = document.querySelectorAll('.rotator__case')
-let i = 0;
-setInterval(() => {
-    rotator[i].classList.remove('rotator__case_active')
-    if (i < rotator.length - 1) {
-        rotator[i].nextElementSibling.classList.add('rotator__case_active')
-        i++
-    }
-    else {
-        rotator[0].classList.add('rotator__case_active')
-        i = 0
-    }
-}, 1000)
+const rotators = document.querySelectorAll('.rotator');
+rotators.forEach((rotator) => {
+    const cases = rotator.querySelectorAll('.rotator__case');
+    let i = 0;
+
+    setInterval(() => {
+        cases[i].classList.remove('rotator__case_active');
+        i = (i + 1) % cases.length;
+        cases[i].classList.add('rotator__case_active');
+    }, 1000);
+});
+
+
+
